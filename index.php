3c889445570cafe2d51a4709fb1c4a44b0bc22cb
Stavarengo <faelsta@gmail.com>
2014-02-10 20:18:08 -0200

    Pages

--- index.php
+++ index.php
@@ -1,2 +1,47 @@
-<?php
-echo "My GitHub Page ------ ";
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!--        <link rel="shortcut icon" href="../../assets/ico/favicon.ico">-->
+
+        <title>PHP Sigep</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="/site/css/twbs/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="/site/css/site.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <div class="starter-template">
+                <h1>Bootstrap starter template</h1>
+
+                <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.
+                </p>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="/site/js/jquery-1.10.2.min.js"></script>
+        <script src="/site/css/twbs/js/bootstrap.min.js"></script>
+    </body>
+</html>
