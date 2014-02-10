dc7b4e5d0092ebd4519a2d6bb0b3ab8f9f261215
Stavarengo <faelsta@gmail.com>
2014-02-10 21:23:26 -0200

    Update pages

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    $addressForm = file_get_contents(__DIR__ . '/_addressForm.html');
+    $remetenteForm = str_replace('${tipo}', 'remetente', $addressForm);
+    $destinatarioForm = str_replace('${tipo}', 'destinatario', $addressForm);
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -10,13 +15,9 @@
 
         <title>PHP Sigep</title>
 
-        <!-- Bootstrap core CSS -->
         <link href="/site/css/twbs/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom styles for this template -->
         <link href="/site/css/site.css" rel="stylesheet">
 
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -27,20 +28,53 @@
 
         <div class="container">
 
-            <div class="starter-template">
-                <h1>Bootstrap starter template</h1>
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Remetente</h3>
+                    <?php echo $remetenteForm ?>
+                </div>
+            </div>
 
-                <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.
-                </p>
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Destinatário</h3>
+                    <?php echo $destinatarioForm ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Embalagem</h3>
+
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label for="peso">Peso total</label>
+                                <input type="text" class="form-control" id="peso" name="peso">
+                                <span class="help-block">Embalagem + mercadoria.</span>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label for="tipoTransporte">Tipo de transporte</label>
+                                <select class="form-control" name="tipoTransporte">
+                                    <option value="81019">E-sedex</option>
+                                    <option value="41068">Pac</option>
+                                    <option value="40096">Sedex</option>
+                                    <option value="40215">Sedex 10 Envelope</option>
+                                    <option value="10065">Carta</option>
+                                    <option value="40886">Sedex 10 Pacote</option>
+                                    <option value="40878">Sedex Hoje</option>
+                                    <option value="41009">Sedex Agrupado</option>
+                                    <option value="10138">Carta Registrada</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
-        <!-- /.container -->
-
-
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
         <script src="/site/js/jquery-1.10.2.min.js"></script>
         <script src="/site/css/twbs/js/bootstrap.min.js"></script>
     </body>
