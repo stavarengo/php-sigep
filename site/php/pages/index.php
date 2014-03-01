01809d060f61b0e83f2e515b01db1686f27e6289
Stavarengo <faelsta@gmail.com>
2014-03-01 00:25:49 -0300

    adicionado analytics

--- site/php/pages/index.php
+++ site/php/pages/index.php
@@ -128,5 +128,15 @@
         <script src="<?php echo $baseUrl ?>/site/css/twbs/js/bootstrap.min.js"></script>
         <script src="<?php echo $baseUrl ?>/site/js/highlight/highlight.pack.js"></script>
         <script src="<?php echo $baseUrl ?>/site/js/app.js"></script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-12926898-13', 'stavarengo.com');
+            ga('send', 'pageview');
+
+        </script>
     </body>
 </html>
