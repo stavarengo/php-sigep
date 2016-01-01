90ec0fab958b2e621c01a85dcb5c1d337a08e92c
Guilherme Viebig <guilherme@bitloom.net>
2016-01-01 16:32:28 -0200

    rádido -> rápido nos comentários

--- src/PhpSigep/Services/SoapClient/Real.php
+++ src/PhpSigep/Services/SoapClient/Real.php
@@ -51,7 +51,7 @@ class Real implements ServiceInterface
      * Pede para o WebService do Correios calcular o dígito verificador de uma etiqueta.
      * 
      * Se preferir você pode usar o método {@linnk \PhpSigep\Model\Etiqueta::getDv() } para calcular o dígito 
-     * verificador, visto que esse método é mais rádido pois faz o cálculo local sem precisar se comunicar com o
+     * verificador, visto que esse método é mais rápido pois faz o cálculo local sem precisar se comunicar com o
      * WebService.
      * 
      * @param \PhpSigep\Model\GeraDigitoVerificadorEtiquetas $params
