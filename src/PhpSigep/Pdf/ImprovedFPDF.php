9185c2131def940a9bba7b9f1f3c5ec6498a20b4
Anderson <andersonls@users.noreply.github.com>
2016-08-18 16:28:40 -0300

    Add função para desenhar retângulo com bordas arredondadas no FPDF

--- src/PhpSigep/Pdf/ImprovedFPDF.php
+++ src/PhpSigep/Pdf/ImprovedFPDF.php
@@ -1,7 +1,10 @@
 <?php
-namespace PhpSigep\Pdf;
-
-class ImprovedFPDF extends \PhpSigepFPDF
+namespace Sigep\Pdf;
+/**
+ * @author: Stavarengo
+ * @modify Anderson Luiz Silvério <andersonlsilverio@gmail.com>
+ */
+class ImprovedFPDF extends \FPDI
 {
     /**
      * @var int
@@ -16,13 +19,21 @@ class ImprovedFPDF extends \PhpSigepFPDF
     private $javascript;
     private $n_js;
 
+    private $widths;
+    private $aligns;
+    
     function __construct($orientation = 'P', $unit = 'mm', $size = 'A4')
     {
         parent::__construct($orientation, $unit, $size);
         $this->lineHeightPadding = 30 / $this->k;
         $this->SetAutoPageBreak(false);
 
-        stream_wrapper_register("var", 'PhpSigep\Pdf\VariableStream') or die("Failed to register protocol");
+        $existed = in_array("var", stream_get_wrappers());
+ 	if (!$existed) {
+		stream_wrapper_register("var", '\Sigep\Pdf\VariableStream') or die("Failed to register protocol");
+		//stream_wrapper_unregister("var");
+	}
+        
     }
 
     public function _($str)
@@ -199,6 +210,174 @@ class ImprovedFPDF extends \PhpSigepFPDF
             $this->_out('/Names <</JavaScript '.($this->n_js).' 0 R>>');
         }
     }
+    
+    function SetDash($black=null, $white=null) {
+        if($black!==null)
+            $s=sprintf('[%.3F %.3F] 0 d',$black*$this->k,$white*$this->k);
+            else
+                $s='[] 0 d';
+                $this->_out($s);
+    }
+    
+    function SetWidths($w)
+    {
+        //Set the array of column widths
+        $this->widths=$w;
+    }
+    
+    function SetAligns($a)
+    {
+        //Set the array of column alignments
+        $this->aligns=$a;
+    }
+    
+    function Row($data)
+    {
+        //Calculate the height of the row
+        $nb=0;
+        for($i=0;$i<count($data);$i++) {
+            $nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+        }
+        $h=5*$nb;
+        //Issue a page break first if needed
+        $this->CheckPageBreak($h);
+        //Draw the cells of the row
+        for($i=0;$i<count($data);$i++)
+        {
+            $w=$this->widths[$i];
+            $a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+            //Save the current position
+            $x=$this->GetX();
+            $y=$this->GetY();
+            //Draw the border
+            $this->Rect($x,$y,$w,$h);
+            //Print the text
+            $this->MultiCell($w,5,$data[$i],0,$a);
+            //Put the position to the right of the cell
+            $this->SetXY($x+$w,$y);
+        }
+        //Go to the next line
+        $this->Ln($h);
+    }
+    
+    function CheckPageBreak($h)
+    {
+        //If the height h would cause an overflow, add a new page immediately
+        if($this->GetY()+$h>$this->PageBreakTrigger)
+            $this->AddPage($this->CurOrientation);
+    }
+    
+    function NbLines($w,$txt)
+    {
+        //Computes the number of lines a MultiCell of width w will take
+        $cw=&$this->CurrentFont['cw'];
+        if($w==0) {
+            $w=$this->w-$this->rMargin-$this->x;
+        }
+        $wmax=($w-2*$this->cMargin)*1000/$this->FontSize;
+        $s=str_replace("\r",'',$txt);
+        $nb=strlen($s);
+        if($nb>0 and $s[$nb-1]=="\n") {
+            $nb--;
+        }
+        $sep=-1;
+        $i=0;
+        $j=0;
+        $l=0;
+        $nl=1;
+        while($i<$nb)
+        {
+            $c=$s[$i];
+            if($c=="\n")
+            {
+                $i++;
+                $sep=-1;
+                $j=$i;
+                $l=0;
+                $nl++;
+                continue;
+            }
+            if($c==' ') {
+                $sep=$i;
+            }
+            $l+=$cw[$c];
+            if($l>$wmax)
+            {
+                if($sep==-1)
+                {
+                    if($i==$j)
+                        $i++;
+                }
+                else
+                    $i=$sep+1;
+                    $sep=-1;
+                    $j=$i;
+                    $l=0;
+                    $nl++;
+            }
+            else
+                $i++;
+        }
+        return $nl;
+    }
+    
+    function RoundedRect($x, $y, $w, $h, $r, $corners = '1234', $style = '')
+    {
+        $k = $this->k;
+        $hp = $this->h;
+        if($style=='F')
+            $op='f';
+        elseif($style=='FD' || $style=='DF')
+            $op='B';
+        else
+            $op='S';
+            $MyArc = 4/3 * (sqrt(2) - 1);
+            $this->_out(sprintf('%.2F %.2F m',($x+$r)*$k,($hp-$y)*$k ));
+    
+        $xc = $x+$w-$r;
+        $yc = $y+$r;
+        $this->_out(sprintf('%.2F %.2F l', $xc*$k,($hp-$y)*$k ));
+        if (strpos($corners, '2')===false)
+            $this->_out(sprintf('%.2F %.2F l', ($x+$w)*$k,($hp-$y)*$k ));
+        else
+            $this->_Arc($xc + $r*$MyArc, $yc - $r, $xc + $r, $yc - $r*$MyArc, $xc + $r, $yc);
+
+        $xc = $x+$w-$r;
+        $yc = $y+$h-$r;
+        $this->_out(sprintf('%.2F %.2F l',($x+$w)*$k,($hp-$yc)*$k));
+        if (strpos($corners, '3')===false)
+            $this->_out(sprintf('%.2F %.2F l',($x+$w)*$k,($hp-($y+$h))*$k));
+        else
+            $this->_Arc($xc + $r, $yc + $r*$MyArc, $xc + $r*$MyArc, $yc + $r, $xc, $yc + $r);
+
+        $xc = $x+$r;
+        $yc = $y+$h-$r;
+        $this->_out(sprintf('%.2F %.2F l',$xc*$k,($hp-($y+$h))*$k));
+        if (strpos($corners, '4')===false)
+            $this->_out(sprintf('%.2F %.2F l',($x)*$k,($hp-($y+$h))*$k));
+        else
+            $this->_Arc($xc - $r*$MyArc, $yc + $r, $xc - $r, $yc + $r*$MyArc, $xc - $r, $yc);
+
+        $xc = $x+$r ;
+        $yc = $y+$r;
+        $this->_out(sprintf('%.2F %.2F l',($x)*$k,($hp-$yc)*$k ));
+        if (strpos($corners, '1')===false)
+        {
+            $this->_out(sprintf('%.2F %.2F l',($x)*$k,($hp-$y)*$k ));
+            $this->_out(sprintf('%.2F %.2F l',($x+$r)*$k,($hp-$y)*$k ));
+        }
+        else
+            $this->_Arc($xc - $r, $yc - $r*$MyArc, $xc - $r*$MyArc, $yc - $r, $xc, $yc - $r);
+            $this->_out($op);
+    }
+    
+    function _Arc($x1, $y1, $x2, $y2, $x3, $y3)
+    {
+        $h = $this->h;
+        $this->_out(sprintf('%.2F %.2F %.2F %.2F %.2F %.2F c ', $x1*$this->k, ($h-$y1)*$this->k,
+                $x2*$this->k, ($h-$y2)*$this->k, $x3*$this->k, ($h-$y3)*$this->k));
+    }
+    
 }
 
 //Stream handler to read from global variables
