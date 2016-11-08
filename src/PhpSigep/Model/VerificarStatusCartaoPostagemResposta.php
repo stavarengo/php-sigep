a5e8fd92526967c621a861326290bd739c6d553a
davidalves1 <alves.david@outlook.com>
2016-11-08 08:42:42 -0200

    Correção nos comentários

--- src/PhpSigep/Model/VerificarStatusCartaoPostagemResposta.php
+++ src/PhpSigep/Model/VerificarStatusCartaoPostagemResposta.php
@@ -7,16 +7,25 @@ namespace PhpSigep\Model;
  */
 class VerificaStatusCartaoPostagemResposta extends AbstractModel
 {
+    /**
+     * @var string
+     */
     protected $return;
 
+    /**
+     * @param $return
+     * @return $this;
+     */
     public function setReturn($return)
     {
         $this->return = $return;
 
         return $this;
     }
-
     
+    /**
+     * @return string
+     */
     public function getReturn()
     {
         return $this->return;
