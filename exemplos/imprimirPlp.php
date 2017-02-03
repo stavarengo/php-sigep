e833eb8373687f0ee38416d7652af58c50129a9e
mozgbrasil <mozgbrasil@gmail.com>
2017-02-03 11:25:47 -0200

    Update imprimirPlp.php

--- exemplos/imprimirPlp.php
+++ exemplos/imprimirPlp.php
@@ -5,4 +5,4 @@ require_once __DIR__ . '/bootstrap-exemplos.php';
 $params = include __DIR__ . '/helper-criar-pre-lista.php';
 
 $pdf  = new \PhpSigep\Pdf\ListaDePostagem($params, time());
-$pdf->render($params);
+$pdf->render('I');
