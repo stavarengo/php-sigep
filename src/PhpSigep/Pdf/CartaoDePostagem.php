ec711669aeb9bb65026c766c18ace40e29a841b0
lhpalacio <lhpalacio@outlook.com>
2017-05-10 18:31:00 -0300

    Atualiza códigos de serviço do SEDEX e PAC sem contrato

--- src/PhpSigep/Pdf/CartaoDePostagem.php
+++ src/PhpSigep/Pdf/CartaoDePostagem.php
@@ -232,7 +232,7 @@ class CartaoDePostagem
 
                 switch ($servicoDePostagem->getCodigo()) {
                     case ServicoDePostagem::SERVICE_PAC_41068:
-                    case ServicoDePostagem::SERVICE_PAC_41106:
+                    case ServicoDePostagem::SERVICE_PAC_04510:
                     case ServicoDePostagem::SERVICE_PAC_CONTRATO_41211:
                     case ServicoDePostagem::SERVICE_PAC_CONTRATO_AGENCIA:
                     case ServicoDePostagem::SERVICE_PAC_GRANDES_FORMATOS:
