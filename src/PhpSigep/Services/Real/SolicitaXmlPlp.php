c54571add7738a006cf31107b4d6e016e436230f
brunorthomaz <brunorthomaz@gmail.com>
2017-09-05 13:27:06 -0300

    Acertando encodificação (#174)
    Com a remoção do CDATA caracteres especiais são convertidos de forma incorreta

--- src/PhpSigep/Services/Real/SolicitaXmlPlp.php
+++ src/PhpSigep/Services/Real/SolicitaXmlPlp.php
@@ -43,7 +43,8 @@ class SolicitaXmlPlp
 
             if (is_string($r->return)) {
                 libxml_use_internal_errors(true);
-                $xml = simplexml_load_string($r->return, \SimpleXMLElement::class, LIBXML_NOCDATA);
+                $xmlString = iconv('utf-8', 'ISO-8859-1', $r->return);
+                $xml = simplexml_load_string($xmlString, \SimpleXMLElement::class, LIBXML_NOCDATA);
                 if ($xml instanceof \SimpleXMLElement) {
                     $objectToarray = json_decode(json_encode($xml), true);
 
