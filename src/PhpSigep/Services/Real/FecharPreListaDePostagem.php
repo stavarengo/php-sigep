cbc8d45e20a921e27cacdf96e7ea59e0c319dfaa
Gustavo Bobrow <gustvao@gmail.com>
2018-01-22 21:52:32 -0200

    adjusting ecoding for ISO on XML and valor_declarado to be string and respect manuals for brazilian D,##

--- src/PhpSigep/Services/Real/FecharPreListaDePostagem.php
+++ src/PhpSigep/Services/Real/FecharPreListaDePostagem.php
@@ -81,7 +81,7 @@ class FecharPreListaDePostagem
         $writer->openMemory();
         $writer->setIndentString("");
         $writer->setIndent(false);
-        $writer->startDocument('1.0', 'UTF-8');
+        $writer->startDocument('1.0', 'ISO-8859-1');
 
         $writer->startElement('correioslog');
         $writer->writeElement('tipo_arquivo', 'Postagem');
@@ -265,7 +265,7 @@ class FecharPreListaDePostagem
             if ($servicoAdicional->getCodigoServicoAdicional() != ServicoAdicional::SERVICE_REGISTRO) {
                 $writer->writeElement('codigo_servico_adicional', $servicoAdicional->getCodigoServicoAdicional());
                 if ($servicoAdicional->getCodigoServicoAdicional() == ServicoAdicional::SERVICE_VALOR_DECLARADO) {
-                    $writer->writeElement('valor_declarado', (float)$servicoAdicional->getValorDeclarado());
+                    $writer->writeElement('valor_declarado', (string)$servicoAdicional->getValorDeclarado());
                 }
             }
         }
