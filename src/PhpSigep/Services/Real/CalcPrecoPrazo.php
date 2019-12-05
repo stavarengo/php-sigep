c47c6999635a7f337b28cb0f3c11367fa2990ddf
Rodrigo Laurindo <rblaurin@gmail.com>
2019-12-05 17:09:45 -0300

    Update CalcPrecoPrazo.php

--- src/PhpSigep/Services/Real/CalcPrecoPrazo.php
+++ src/PhpSigep/Services/Real/CalcPrecoPrazo.php
@@ -181,11 +181,15 @@ class CalcPrecoPrazo
             }
         } else {
             $result->setErrorCode(0);
-            if (isset($r) && is_object($r)) {
-                $result->setErrorMsg('A resposta do Correios não está no formato esperado. Detalhes do problema: "A resposta recebida é um objeto, mas este objeto não possui todos as entradas necessárias."');
+            if (isset($r)) {
+                if (is_object($r)) {
+                    $result->setErrorMsg('A resposta do Correios não está no formato esperado. Detalhes do problema: "A resposta recebida é um objeto, mas este objeto não possui todos as entradas necessárias."');
+                } else {
+                    $result->setErrorMsg('A resposta do Correios não está no formato esperado. Resposta recebida: "' .
+                        $r . '"');
+                }
             } else {
-                $result->setErrorMsg('A resposta do Correios não está no formato esperado. Resposta recebida: "' .
-                    $r . '"');
+                $result->setErrorMsg('Nenhuma resposta foi recebida dos correios.');
             }
         }
 
