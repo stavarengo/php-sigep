aa1af8729ee900e5764a446449f2cf3270326815
Rômulo Neves <romulo.neves@oi.net.br>
2020-03-03 13:07:14 -0300

    Adicionando classe Proxy para autenticar conexões via proxy para o soapClient()

---
+++ src/PhpSigep/Model/Proxy.php
@@ -0,0 +1,93 @@
+<?php
+namespace PhpSigep\Model;
+
+use PhpSigep\InvalidArgument;
+
+/**
+ * @author: Rômulo Neves (https://github.com/romuloneves)
+ */
+class Proxy extends AbstractModel
+{
+
+    /**
+     * @var string
+     */
+    protected $host;
+    /**
+     * @var int
+     */
+    protected $port;
+    /**
+     * @var string
+     */
+    protected $login;
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param int $port
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string $login
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
