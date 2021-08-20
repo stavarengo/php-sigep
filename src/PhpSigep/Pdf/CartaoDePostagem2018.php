3ace5f0137de0dbd7004f25ff2130da0aed5f60e
alisson <alisson.correa@climba.com.br>
2021-08-20 11:53:29 -0300

    Melhoria na remoção dos zeros a esquerda
    Melhoria na remoção dos zeros a esquerda pois quando o numero da nota fiscal é menor que 5 caracteres estava ficando em branco

--- src/PhpSigep/Pdf/CartaoDePostagem2018.php
+++ src/PhpSigep/Pdf/CartaoDePostagem2018.php
@@ -263,7 +263,7 @@ class CartaoDePostagem2018
             $this->pdf->SetFontSize(9);
             //$this->pdf->SetTextColor(51,51,51);
             $nf = $objetoPostal->getDestino()->getNumeroNotaFiscal();
-            $str = $nf > 0 ?  'NF: '. substr($nf,5) : ' ';
+            $str = 'NF: '.preg_replace('/^0+(?!$)/', '', $nf);
             $this->t(15, $str, 1, 'L',  null);
 
             // Contrato
