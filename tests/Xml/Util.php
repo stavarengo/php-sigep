ce1cbbf2ce177c2afd1f71714def99f005a59a09
Gerson Felipe Schwinn <gerson@onehost.com.br>
2022-05-06 18:23:21 -0300

    Removendo métodos não usados

--- tests/Xml/Util.php
+++ tests/Xml/Util.php
@@ -4,26 +4,6 @@ namespace PhpSigep\Test\Xml;
 
 class Util
 {
-    /**
-     * @param string $xml
-     * @return string
-     */
-    public static function removeWhiteSpaces(string $xml)
-    {
-        $obj = simplexml_load_string($xml, null, LIBXML_NOBLANKS);
-
-        return str_replace("\n", "", $obj->asXML());
-    }
-
-    /**
-     * @param string $path
-     * @return string
-     */
-    public static function removeWhiteSpacesFromPath(string $path)
-    {
-        return self::removeWhiteSpaces(file_get_contents($path));
-    }
-
     public static function normalizeXmlForTest(string $xml): string
     {
         $dom = new \DOMDocument();
